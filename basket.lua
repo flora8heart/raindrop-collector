4ff02926b714e2ffc413093c729402cf3224bb27
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-10-06 22:16:26 +0100

    Fix basket
    Add basket image and refactor basket movement code

--- basket.lua
+++ basket.lua
@@ -1,38 +1,46 @@
 Basket = Object:extend()
 
+local scale = 0.25
+
 function Basket:new()
-  -- Create basket variable and set x to 0
+  self.image = love.graphics.newImage("img/basket.png")
   self.x = 10
-  self.speed = 100
-  -- set basket min starting location
+  self.y = 440
+  self.speed = 200
+  -- set basket min boundary
   self.min = 10
-  self.max = love.graphics.getWidth()
+  -- set basket max boundary
+  self.max = love.graphics.getWidth() - self.min
+  self.width = (self.image:getWidth()) * scale -- remember to multiple by the scale value to get the new width value
+  self.height = (self.image:getHeight()) * scale
 end
 
 function Basket:update(dt)
   -- Add controls to basket
   -- Use dt to offset different computer framerate
   if love.keyboard.isDown("right", "d") then
-    -- add right wall boundary
-    if self.x >= self.min and self.x < self.max - (self.speed + self.min) then
-      self.x = self.x + self.speed * dt
-      print(self.x)
-    end
+    self.x = self.x + self.speed * dt
   elseif love.keyboard.isDown("left", "a") then
-    -- add left wall boundary
-    if self.x - self.speed > 0 then
-      self.x = self.x - self.speed * dt
-      print(self.x)
-    else
-      -- use math.max to create fuzziness zone so the min value is at self.min, this is to ensure that basket doesn't automatically snap to the minimum position.
-      if self.x - self.speed < 0 then
-        self.x = math.max(self.min, self.x - 100 * dt)
-        print(self.x)
-      end
-    end
+    self.x = self.x - self.speed * dt
+  end
+
+  -- Set basket movement boundaries
+  -- Get window width
+  local window_width = love.graphics.getWidth()
+
+  -- Prevent basket moving past left boundary
+  if self.x < self.min then
+    self.x = self.min
+    -- print(self.min, self.x)
+  -- Prevent basket moving past right boundary
+  elseif self.x > self.max - self.width then
+    self.x = self.max - self.width
+    -- print(self.x, self.max)
   end
 end
 
 function Basket:draw()
-  love.graphics.rectangle("line", self.x, 500, 100, 50)
+  -- love.graphics.rectangle("line", self.x, 500, 100, 50)
+  -- love.graphics.draw( image, x, y , rotation, xScale , yScale
+  love.graphics.draw(self.image, self.x, self.y, 0, scale, scale)
 end
