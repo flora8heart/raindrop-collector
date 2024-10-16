4bf549af7a4815c5a01e685267a32fa1ab84bb8a
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-10-16 22:09:52 +0100

    Add intro and playing state to update

--- main.lua
+++ main.lua
@@ -39,30 +39,37 @@ function love.load()
 end
 
 function love.update(dt)
-  cloud:update(dt, score) -- passing score into cloud:update() to change cloud speed according to certain conditions
-  basket:update(dt, score)
-
-  -- iterate through listOfRaindrops and update raindrop in listOfRaindrops
-  for i, v in ipairs(listOfRaindrops) do
-    -- print("v", v)
-    v:update(dt)
-
-    -- Check collision of raindrop with basket
-    v:checkCollision(basket)
-
-    -- if collision is detected then remove raindrop
-    if v.collision then
-      sounds.raindrop:play()
-      -- print("remove raindrop!")
-      table.remove(listOfRaindrops, i)
-      -- Update score counter
-      score = score + 1
-      print("score", score)
+  -- if game state is intro -> conditions to run intro screen
+  if gameState == "intro" then
+    if love.keyboard.isDown("return", "space") then
+      gameState = "playing"
     end
+  -- if game state is playing --> conditions to run playing screen
+  elseif gameState == "playing" then
+    cloud:update(dt, score) -- passing score into cloud:update() to change cloud speed according to certain conditions
+    basket:update(dt, score)
 
-    -- remove raindrop from screen if exceeds screen
-    if v.missed then
-      table.remove(listOfRaindrops, i)
+    -- iterate through listOfRaindrops and update raindrop in listOfRaindrops
+    for i, v in ipairs(listOfRaindrops) do
+      -- print("v", v)
+      v:update(dt)
+
+      -- Check collision of raindrop with basket
+      v:checkCollision(basket)
+
+      -- if collision is detected then remove raindrop
+      if v.collision then
+        sounds.raindrop:play()
+        -- print("remove raindrop!")
+        table.remove(listOfRaindrops, i)
+        -- Update score counter
+        score = score + 1
+        print("score", score)
+      end
+      -- remove raindrop from screen if exceeds screen
+      if v.missed then
+        table.remove(listOfRaindrops, i)
+      end
     end
   end
 end
